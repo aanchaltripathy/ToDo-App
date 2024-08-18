ad5816c7dedfe92a5f698e95b14f248123c01ef2
aanchaltripathy <86507308+aanchaltripathy@users.noreply.github.com>
2024-08-18 17:54:08 +0530

    Add files via upload

---
+++ frontend/todo-app/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+  import { Component } from 'react';
+  import UrlController from './component/UrlController';
+  
+  class App extends Component {
+    render() {
+      return(
+        <div className="container"><UrlController /></div>
+      );
+    }
+  }
+export default App;
